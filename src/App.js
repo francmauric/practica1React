8e3fdb2b3aa15c651fcf159c7c4303770c858a14
José Miranda <josemiranda989@gmail.com>
2022-09-22 12:40:26 -0300

    fixed routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 //import logo from './logo.svg';
-import './App.css';
-import Home from './component/Home';
+import "./App.css";
+import Home from "./component/Home";
 /* import About from './component/About';
 import Contact from './component/Contact'; */
-import {BrowserRouter, Link, Route} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-       
-         <Home />
-         <Route path="/" component={Home} /> 
-       
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} /> */}
+    </Routes>
   );
 }
 
